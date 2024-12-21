8ef4386e352eb80ba802a7abeceaa20ee83e277f
johnlester05 <orolajohnlester7@gmail.com>
2024-12-22 01:28:37 +0800

    Add files via upload

---
+++ LA 2.py
@@ -0,0 +1,10 @@
+class ML():
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+        
+    
+hero = ML("Hayabusa", "Assassin")
+
+print(hero.name)
+print(hero.role)
