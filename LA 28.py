d859c0b19107372680018272ed86e1d0ed653ea1
johnlester05 <orolajohnlester7@gmail.com>
2024-12-22 17:40:49 +0800

    Delete LA 28.py

--- LA 28.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-name = "SABABAN_JOHNIVAN"
-root = tk.Tk()
-root.title(f"OOP LA28 {name}")
-root.geometry("500x400")
-root.configure(bg="purple")
-root.configure(bg ="red")
-root.mainloop()
