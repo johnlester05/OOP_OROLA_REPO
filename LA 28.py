162c2b2d01bb27a8b2dfebfcb55e449c80cfc2fa
johnlester05 <orolajohnlester7@gmail.com>
2024-12-22 17:38:59 +0800

    Add files via upload

---
+++ LA 28.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+name = "SABABAN_JOHNIVAN"
+root = tk.Tk()
+root.title(f"OOP LA28 {name}")
+root.geometry("500x400")
+root.configure(bg="purple")
+root.configure(bg ="red")
+root.mainloop()
