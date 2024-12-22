a849c658de1370df2e37702e8e72f798985f8917
johnlester05 <orolajohnlester7@gmail.com>
2024-12-22 17:09:36 +0800

    Add files via upload

---
+++ LA 16.py
@@ -0,0 +1,23 @@
+class Appliances:
+    def __init__(self,brand,model):
+        self.brand = brand
+        self.model = model
+    def operate(self):
+        print(f"{self.brand} Operating!")
+       
+class WashingMachine(Appliances):
+    def operate(self):
+        print(f"{self.brand}: Washing clothes!")
+class Refrigerator(Appliances):
+    def operate(self):
+        print(f"{self.brand} {self.model}: Keeping food cold!")
+class Microwave(Appliances):
+    def operate(self):
+        print(f"{self.brand} {self.model}: Heating food! ")
+
+tcl = WashingMachine("TCL", "TWA 95")
+samsung = Refrigerator("Samsung", "RS64")
+whirpool = Microwave("Whirpool", "MWX 203")
+
+for appl in [tcl, samsung, whirpool]:
+    appl.operate()
